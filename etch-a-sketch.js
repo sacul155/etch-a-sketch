3878ec7f352739fbfea222d95767d60ac9c3195e
sacul155 <cr1sram1rez107@gmail.com>
2026-03-16 20:31:58 -0400

    Custom grid size functionality added
    Added a button in JavaSript with text "New Game" and a button event listener that executes a new function when
    clicked.  This new function, updateGrid() will remove all old squares and replace them with the value the user
    inputs from the prompt.  This is done by selecting all squares with document.querySelectorAll(."square") and
    adding them to a new node list, then looping through that list with oldSquares.forEach(square => square.remove()).
    Once the old grid is removed, then the prompt appears and the createGrid() function is called with new value.
    
    Initial grid size is 50x50.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,21 @@
 const content = document.querySelector(".container");
+const body = document.querySelector("body");
+
+const button = document.createElement("button");
+body.insertBefore(button, content);
+button.className = "refresh";
+button.textContent = "New Game";
+//button added, need to add event listener so when button is clicked, prompt pops up, user enters new grid size,
+//old grid gets deleted, new one gets made
+
 
 function createGrid(gridSize) {
-    for (let i = 0; i < gridSize; i++) {
+    let totalSquares = gridSize * gridSize;
+    for (let i = 0; i < totalSquares; i++) {
         const square = document.createElement("div");
-        square.className("square");
-        square.style.height = `${100 / Math.sqrt(gridSize)}%`;
-        square.style.width = `${100 / Math.sqrt(gridSize)}%`;
+        square.className = "square";
+        square.style.height = `${100 / gridSize}%`;
+        square.style.width = `${100 / gridSize}%`;
         square.addEventListener("mouseover", function changeColor() {
             square.style.backgroundColor = "black";
         })
@@ -13,4 +23,13 @@ function createGrid(gridSize) {
     }
 }
 
-createGrid(1000);
+createGrid(50);
+
+function updateGrid() {
+    const oldSquares = document.querySelectorAll(".square");
+    oldSquares.forEach(square => square.remove());
+    newSize = prompt("Enter new grid size:");
+    createGrid(newSize);
+}
+button.addEventListener('click', updateGrid);
+
