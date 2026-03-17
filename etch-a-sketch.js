42ce78f4f169a3b2dc79c99e45bf675837036c28
sacul155 <cr1sram1rez107@gmail.com>
2026-03-16 20:47:28 -0400

    Comments added to JS file

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,14 +1,17 @@
+//Variable initialization for div.container and <body> elements
 const content = document.querySelector(".container");
 const body = document.querySelector("body");
 
+//Variable initialization for button element, inserts element before div.container so button appears above grid
 const button = document.createElement("button");
 body.insertBefore(button, content);
 button.className = "refresh";
 button.textContent = "New Game";
-//button added, need to add event listener so when button is clicked, prompt pops up, user enters new grid size,
-//old grid gets deleted, new one gets made
 
-
+//This function makes a new grid inside div.container made up of gridSize elements
+//The for loop iterates one time for every square in the grid
+//Height and width are sized to the same percentage of the parent container's dimensions
+//Event is added to every square in the grid that changes its color when hovered over
 function createGrid(gridSize) {
     let totalSquares = gridSize * gridSize;
     for (let i = 0; i < totalSquares; i++) {
@@ -23,13 +26,19 @@ function createGrid(gridSize) {
     }
 }
 
+//Initial grid size when page is loaded
 createGrid(50);
 
+//This function deletes all old square elements, removing the old grid
+//The function then prompts the user for a new grid size, with the number being the dimensions of the grid
+//The createGrid() function is called again with the new size from user
 function updateGrid() {
     const oldSquares = document.querySelectorAll(".square");
     oldSquares.forEach(square => square.remove());
     newSize = prompt("Enter new grid size:");
     createGrid(newSize);
 }
+
+//Calls updateGrid() function when button is clicked
 button.addEventListener('click', updateGrid);
 
